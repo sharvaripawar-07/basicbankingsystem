e538181eab558789364ceb427ab77d669a175b33
sharvaripawar-07 <85942631+sharvaripawar-07@users.noreply.github.com>
2021-06-18 10:43:29 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -9,7 +9,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel = "stylesheet" type="text/css" href="css/style.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
 </head>
 <body>
 <!-- navbar -->
