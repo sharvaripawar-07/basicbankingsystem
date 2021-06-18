9428ce3c40199bc91f4399517f434ae4eabd0680
sharvaripawar-07 <85942631+sharvaripawar-07@users.noreply.github.com>
2021-06-18 10:44:53 +0530

    Update about.php

--- about.php
+++ about.php
@@ -11,8 +11,6 @@
   <link rel="stylesheet" href="css/style.css">
   <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet"> 
-
-</style>
 </head>
 <body>
  
